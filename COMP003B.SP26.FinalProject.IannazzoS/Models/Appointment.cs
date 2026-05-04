f40c9b8877d328d0c424ec8cb17f3599324281f4
samian <sam.iannazzo@yahoo.com>
2026-05-03 20:49:44 -0700

    created model for appointments

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Models/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.SP26.FinalProject.IannazzoS.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int SymptomId { get; set; }
+
+        // nullable nav properties
+        public virtual Patient? Patient { get; set; }
+        public virtual Symptom? Symptom { get; set; }
+    }
+}
