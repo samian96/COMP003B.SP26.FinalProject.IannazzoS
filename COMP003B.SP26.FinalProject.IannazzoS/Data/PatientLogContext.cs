4f8006486bb474c68eaa8e65cf6c3674356c870f
samian <sam.iannazzo@yahoo.com>
2026-05-03 22:00:59 -0700

    Created PatientLogContext for Db

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Data/PatientLogContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using COMP003B.SP26.FinalProject.IannazzoS.Models;
+namespace COMP003B.SP26.FinalProject.IannazzoS.Data
+{
+    public class PatientLogContext : DbContext
+    {
+        public PatientLogContext(DbContextOptions<PatientLogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Symptom> Symptoms { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+
+}
