ed0252df5e5f2abb6fd00fc7cc7f5953ddff5abd
samian <sam.iannazzo@yahoo.com>
2026-05-03 20:47:15 -0700

    created model for symptoms

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Models/Symptom.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.SP26.FinalProject.IannazzoS.Models
+{
+    public class Symptom
+    {
+        public int SymptomId { get; set; }
+
+        [Required]
+
+        public string Name { get; set; }
+
+        // navigation 
+
+        public virtual ICollection<Appointment>? Appointments { get; set; }
+    }
+}
