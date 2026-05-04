320c9daf2af80e400a7286e444da7c37b3229c93
samian <sam.iannazzo@yahoo.com>
2026-05-03 20:43:48 -0700

    Created model for patient

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.IannazzoS.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        // collection navigation
+        public virtual ICollection<Appointment>? Appointments { get; set; }
+    }
+}
