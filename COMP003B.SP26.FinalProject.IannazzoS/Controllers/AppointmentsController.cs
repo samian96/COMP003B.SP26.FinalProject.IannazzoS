8665b61049ab37adacf959878485e26d28b95066
samueliannazzo <samueliannazzo@LEM326-04>
2026-05-04 18:40:09 -0700

    routed api to appointment controller

--- COMP003B.SP26.FinalProject.IannazzoS/Controllers/AppointmentsController.cs
+++ COMP003B.SP26.FinalProject.IannazzoS/Controllers/AppointmentsController.cs
@@ -10,6 +10,8 @@ using COMP003B.SP26.FinalProject.IannazzoS.Models;
 
 namespace COMP003B.SP26.FinalProject.IannazzoS.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class AppointmentsController : Controller
     {
         private readonly PatientLogContext _context;
