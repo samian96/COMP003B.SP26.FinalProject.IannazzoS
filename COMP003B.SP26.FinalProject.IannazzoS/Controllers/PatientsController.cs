3c802965279db5ebc3becb975e7930d1990b15d8
samian <sam.iannazzo@yahoo.com>
2026-05-04 20:48:10 -0700

    re created the database

--- COMP003B.SP26.FinalProject.IannazzoS/Controllers/PatientsController.cs
+++ COMP003B.SP26.FinalProject.IannazzoS/Controllers/PatientsController.cs
@@ -41,8 +41,8 @@ namespace COMP003B.SP26.FinalProject.IannazzoS.Controllers
             }
             // get patients symptoms
             ViewBag.Symptoms = from p in _context.Patients
-                               join a in _context.Appointments on p.PatientId equals p.PatientId
-                               join s in _context.Symptoms on s.SymptomId equals s.SymptomId
+                               join a in _context.Appointments on p.PatientId equals a.PatientId
+                               join s in _context.Symptoms on a.SymptomId equals s.SymptomId
                                where p.PatientId == id
                                select s;
 
