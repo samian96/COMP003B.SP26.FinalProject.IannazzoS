a76ca144cb3aa9e33e6d457957ba1b4ec5c4406c
samian <sam.iannazzo@yahoo.com>
2026-05-06 15:38:03 -0700

    created seperate model for API

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Controllers/PatientLog.cs
@@ -0,0 +1,13 @@
+﻿using COMP003B.SP26.FinalProject.IannazzoS.Data;
+using COMP003B.SP26.FinalProject.IannazzoS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.SP26.FinalProject.IannazzoS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PatientLog : Controller
+    {
+
+    }
+}
---
+++ COMP003B.SP26.FinalProject.IannazzoS/Models/API/ApiPatient.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP26.FinalProject.IannazzoS.Models.API
+{
+    public class ApiPatient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public EmailAddressAttribute Email { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string? Refferal { get; set; }
+
+    }
+}
