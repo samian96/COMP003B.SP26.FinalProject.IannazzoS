a405e370a4c61d0957db707f7452ff8d2e6816e5
samian <sam.iannazzo@yahoo.com>
2026-05-06 19:37:04 -0700

    added more new ApiPatients

--- COMP003B.SP26.FinalProject.IannazzoS/Data/PatientInfo.cs
+++ COMP003B.SP26.FinalProject.IannazzoS/Data/PatientInfo.cs
@@ -6,7 +6,9 @@ namespace COMP003B.SP26.FinalProject.IannazzoS.Data
     {
         public static List<ApiPatient> Patients { get; } = new()
         {
-            new ApiPatient {Id = 1, Name ="Sam", Age = 30, Medicine = "Aspirin", AppointmentDate = DateTime.UtcNow }
+            new ApiPatient {Id = 1, Name ="Sam", Age = 30, Medicine = "Aspirin", AppointmentDate = DateTime.UtcNow },
+            new ApiPatient {Id = 2, Name ="John", Age = 46, Medicine = "Venlafaxine", AppointmentDate = DateTime.UtcNow },
+            new ApiPatient {Id = 3, Name ="Haylee", Age = 28, Medicine = "Melatonin", AppointmentDate = DateTime.UtcNow, Refferal = "Sam" }
         };
     }
 }
