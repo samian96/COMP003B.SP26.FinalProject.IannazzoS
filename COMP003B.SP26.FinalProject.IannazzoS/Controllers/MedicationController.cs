a5ad27a00685819c00a933621df8a2727ecc0a78
samian <sam.iannazzo@yahoo.com>
2026-05-06 21:06:13 -0700

    created controller for medications

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Controllers/MedicationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.SP26.FinalProject.IannazzoS.Models;
+namespace COMP003B.SP26.FinalProject.IannazzoS.Controllers
+{
+    public class MedicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            var medications = new List<Medication> {
+                new Medication { Id = 1, Name = "Aspirin", Count = 10, Description = "400mg Aspirin for mild pain relief.", PrescriptionDate = DateTime.Now, DoctorNote = "Take as needed for pain 3 times a day." }
+
+            };
+            return View(medications);
+        }
+    }
+}
