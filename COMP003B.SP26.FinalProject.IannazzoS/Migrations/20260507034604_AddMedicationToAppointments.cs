43b44a1a9acef51ee56e384aea31a9186dd2d344
samian <sam.iannazzo@yahoo.com>
2026-05-06 20:51:11 -0700

    created model for medication

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Migrations/20260507034604_AddMedicationToAppointments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP003B.SP26.FinalProject.IannazzoS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMedicationToAppointments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppointmentDate",
+                table: "Appointments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AppointmentDate",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- COMP003B.SP26.FinalProject.IannazzoS/Models/Appointment.cs
+++ COMP003B.SP26.FinalProject.IannazzoS/Models/Appointment.cs
@@ -7,9 +7,12 @@ namespace COMP003B.SP26.FinalProject.IannazzoS.Models
         public int PatientId { get; set; }
         public int SymptomId { get; set; }
 
+        public int MedicationId { get; set; }
+
 
         // nullable nav properties
         public virtual Patient? Patient { get; set; }
         public virtual Symptom? Symptom { get; set; }
+        public virtual Medication? Medication { get; set; }
     }
 }
