ff42d211b7fecb04bd289ed71f6fd1ea6a08094f
samian <sam.iannazzo@yahoo.com>
2026-05-07 20:57:34 -0700

    migration for update

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Migrations/20260508035641_RemovedData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP003B.SP26.FinalProject.IannazzoS.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MedicationId",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MedicationId",
+                table: "Appointments");
+        }
+    }
+}
