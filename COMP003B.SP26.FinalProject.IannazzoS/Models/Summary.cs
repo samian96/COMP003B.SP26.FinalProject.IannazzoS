79b8cb09f1f0003d0419c5a27cf39e070b878137
samian <sam.iannazzo@yahoo.com>
2026-05-07 21:47:14 -0700

    createde model for summary

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Models/Summary.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.SP26.FinalProject.IannazzoS.Models
+{
+    public class Summary
+    {
+        public int Id { get; set; }
+        [Required]
+        [Range(1,10)]
+        public int Article { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+    }
+}
