58b22db0495fe1b0293cf664f934d13d4310c72b
samian <sam.iannazzo@yahoo.com>
2026-05-07 22:11:18 -0700

    created contextcontroller

---
+++ COMP003B.SP26.FinalProject.IannazzoS/Controllers/ContextController.cs
@@ -0,0 +1,26 @@
+﻿using COMP003B.SP26.FinalProject.IannazzoS.Models;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Tracing;
+using System.Reflection;
+namespace COMP003B.SP26.FinalProject.IannazzoS.Controllers
+{
+    [Route("context")]
+    public class ContextController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            var summary = new List<Summary>
+            {
+                new Summary {Id = 1, Article = 1, Title = "Appointments", Description = "Check to see if you have an appointment scheduled, or schedule a new appointment for yourself"}
+            };
+            return View(summary);
+        }
+        public IActionResult Summary(int id)
+        {
+            var details = new Summary { Id = id, Title = $"Summary {id}", Description = "This is a test" };
+            return View(details);
+        }
+    }
+}
