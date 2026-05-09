c5d3127654ac2447bff77c109f0f5025591b5345
samian <sam.iannazzo@yahoo.com>
2026-05-08 18:14:56 -0700

    added new summary

--- COMP003B.SP26.FinalProject.IannazzoS/Controllers/ContextController.cs
+++ COMP003B.SP26.FinalProject.IannazzoS/Controllers/ContextController.cs
@@ -13,14 +13,16 @@ namespace COMP003B.SP26.FinalProject.IannazzoS.Controllers
         {
             var summary = new List<Summary>
             {
-                new Summary {Id = 1, Article = 1, Title = "Appointments", Description = "Check to see if you have an appointment scheduled, or schedule a new appointment for yourself"}
+                new Summary {Id = 1, Article = 1, Title = "Appointments", Description = "Check to see if you have an appointment scheduled, or schedule a new appointment for yourself"},
+
+                new Summary {Id = 2, Article = 2, Title = "Patients", Description = "Check to see if you already have a patient account or need to create a new patient account."}
             };
             return View(summary);
         }
         public IActionResult Summary(int id)
         {
-            var details = new Summary { Id = id, Title = $"Summary {id}",};
-            return RedirectToAction("Index", "Appointments");
+            var details = new Summary { Id = id, Title = $"Summary {id}", Description = "This area should be filled with information on the article",  };
+            return View(details);
         }
     }
 }
